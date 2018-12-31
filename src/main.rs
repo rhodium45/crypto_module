57f53a76ea20a8d77cd6336ed7461326da61f8de
Drilon Buzuku <drilon.buku@gmail.com>
2018-12-31 13:55:13 +0100

    added price formatting

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,19 @@ struct Ticker {
 }
 
 fn main() {
+    // gets a JSON resp like
+    // {"ticker":{"base":"BTC","target":"USD","price":"443.78078", .....},"timestamp":1399490941,"success":true,"error":""}
     let resp: Crypto = reqwest::get("https://api.cryptonator.com/api/ticker/btc-usd")
         .expect("Could not make request")
         .json().expect("Could not read json");
     
     let ticker = resp.ticker;
-    // let price = ticker.price.parse::<f32>().map(|n| n + 0.0);
-    let price = format!("{}", &ticker.price);
+
+    // Converts string into float 
+    let conv_price: f64 = ticker.price.parse().unwrap();
+
+    let price = format!("{:.*}", 2, conv_price);
+    let formatted_price = format!("{}", &price);
     let target = format!("{}", &ticker.target);
-    println!("{} {}", price, target);
+    println!(" {} {}", formatted_price, target);
 }
