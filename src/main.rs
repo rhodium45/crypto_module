84e68a4db0015bffc083e56112985a550ed65231
Drilon Buzuku <drilon.buku@gmail.com>
2019-03-19 17:47:13 +0100

    Add better error handling

--- src/main.rs
+++ src/main.rs
@@ -26,18 +26,20 @@ struct Ticker {
 fn main() {
     let config = config::get_config();
 
-    let resp: Crypto = make_request(create_request_url(config.crypto_iso, config.fiat_iso));
-    print_crypto(config.crypto_logo, format_price(resp.ticker.price), format_target_currency(resp.ticker.target));
+    let resp: Result<Crypto, reqwest::Error> = make_request(create_request_url(config.crypto_iso, config.fiat_iso));
+
+    match resp {
+        Err(e) => handler(e),
+        Ok(resp) => print_crypto(config.crypto_logo, format_price(resp.ticker.price), format_target_currency(resp.ticker.target)),
+    }
 }
 
 fn create_request_url(crypto_iso: String, fiat_iso: String) -> String {
     return format!("https://api.cryptonator.com/api/ticker/{}-{}", &crypto_iso, &fiat_iso);
 }
 
-fn make_request(req_url: String) -> Crypto {
-    return reqwest::get(&req_url)
-        .expect("Could not make request")
-        .json().expect("Could not read json");
+fn make_request(req_url: String) -> Result<Crypto, reqwest::Error> {
+    return reqwest::get(&req_url)?.json()
 }
 
 fn convert_price(crypto_price: String) -> f64 {
@@ -54,4 +56,24 @@ fn format_target_currency(target: String) -> String {
 
 fn print_crypto(logo: String, price: String, target: String) {
     println!("{}{} {}", logo, price, target);
+}
+
+fn handler(e: reqwest::Error) {
+   if e.is_http() {
+       match e.url() {
+           None => println!("No Url given"),
+           Some(url) => println!("Problem making request to: {}", url),
+       }
+   }
+   // Inspect the internal error and output it
+   if e.is_serialization() {
+      let serde_error = match e.get_ref() {
+           None => return,
+           Some(err) => err,
+       };
+       println!("problem parsing information {}", serde_error);
+   }
+   if e.is_redirect() {
+       println!("server redirecting too many times or making loop");
+   }
 }
